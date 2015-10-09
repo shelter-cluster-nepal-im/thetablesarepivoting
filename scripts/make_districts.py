1dfaa794924cba4ee71b2c3a7fc583b56c5e352b
eoglethorpe <ewanogle@gmail.com>
2015-10-09 18:23:26 +0545

    'auto create works'

--- scripts/make_districts.py
+++ scripts/make_districts.py
@@ -7,36 +7,47 @@ import csv
 import shutil
 
 DISTRICTS = [
-"Bhaktapur",
-"Dhading",
-"Dolakha",
-"Gorkha",
-"Kathmandu",
-"Kabhrepalanchok",
-"Lalitpur",
-"Makawanpur",
-"Nuwakot",
-"Okhaldhunga",
-"Ramechhap",
-"Rasuwa",
-"Sindhuli",
-"Sindhupalchok"
+"bhaktapur",
+"dhading",
+"dolakha",
+"gorkha",
+"kathmandu",
+"kabhrepalanchok",
+"lalitpur",
+"makawanpur",
+"nuwakot",
+"okhaldhunga",
+"ramechhap",
+"rasuwa",
+"sindhuli",
+"sindhupalchok"
 ]
 
-BASE_DIR = "/home/ec2-user/thetablesarepivoting/"
-MP = pivot.html
-MD = db.csv
+#AWS: BASE_DIR = "/home/ec2-user/thetablesarepivoting/"
+BASE_DIR = '/Users/ewanog/code/git_repos/thetablesarepivoting/districts/'
+MP = 'dist_pivot.html'
 
-#delete old folders
-for v in DISTRICTS:
-    shutil.rmtree(BASE_DIR + v + '/')
+#pull from SQL to create db file
+conn =  psycopg2.connect(os.environ['dbk'])
+cur = conn.cursor()
+cur.execute("select * from distributions")
+row_names = [desc[0] for desc in cur.description]
 
-#create dirs
+with open(BASE_DIR + 'all/db.csv', 'wb') as f:
+    writer = csv.writer(f)
+    writer.writerow(row_names)
+    for r in cur.fetchall():
+        writer.writerow(r)            
+f.close()
+
+#remove old, create dirs
 for v in DISTRICTS:
-    if not os.path.isdir(BASE_DIR+v)
-	os.makedirs(BASE_DIR+v)
+    if os.path.isdir(BASE_DIR+v):
+        shutil.rmtree(BASE_DIR + v + '/')
+    
+    os.makedirs(BASE_DIR+v)
+
 
 #copy master pivot to folders
 for v in DISTRICTS:
-    shutil.copyfile(BASE_DIR + 'all/' + MP, BASE_DIR + DISTRICT + '/' + MP)
-    shutil.copyfile(BASE_DIR + 'all/' + MP, BASE_DIR + DISTRICT + '/' + MD)
+    shutil.copyfile(BASE_DIR + 'all/' + MP, BASE_DIR + v + '/' + MP)
